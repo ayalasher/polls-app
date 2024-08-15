f58e39dda498b16fcd00ae0c3c6f681428b7f661
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-08-15 19:18:12 +0300

    done for today

--- pappvenv/pollingproject/pollingapp/views.py
+++ pappvenv/pollingproject/pollingapp/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 # Create your views here.
 
 def greeting(request):
     latest_questions_list = Question.objects.order_by("-pub_date")[:5]
     output = ",".join([q.text_question for q in latest_questions_list ])
-    template = loader.get_template("pollingapp/index.html")
+    template = loader.get_template("./tempaltes/pollingapp/index.html")
     context = {
         "latest_question_list":latest_questions_list
     }
@@ -16,7 +17,11 @@ def greeting(request):
     return HttpResponse(output)
 
 def details(request,question_id):
-    return HttpResponse("you are looking at question %s ." % question_id )
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("404 NOT FOUND !!")
+    return render(request,"details.html",{"question":question})
 
 def results(request,question_id):
     response = "you are looking at the reults of the question %s . "
