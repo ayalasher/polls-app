f120b4b3dfd905d764aa0751b88064fc2b8b5010
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-08-22 18:17:30 +0300

    halting the project for the purpose of learning

--- pappvenv/pollingproject/pollingapp/urls.py
+++ pappvenv/pollingproject/pollingapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
+
 urlpatterns =[
     path('',views.greeting , name="greeting" ),
     path('<int:question_id>/',views.details , name="details" ),
-    path('<int:question_id>/', views.results , name="results"  ),
-    path('<int:question_id>/',views.vote, name="vote" )
+    path('<int:question_id>/results/', views.results , name="results"  ),
+    path('<int:question_id>/vote/',views.vote, name="vote" )
 ]
--- pappvenv/pollingproject/pollingapp/views.py
+++ pappvenv/pollingproject/pollingapp/views.py
@@ -7,15 +7,14 @@ from django.http import Http404
 
 def greeting(request):
     latest_questions_list = Question.objects.order_by("-pub_date")[:5]
-    output = ",".join([q.text_question for q in latest_questions_list ])
     template = loader.get_template("index.html")
     context = {
         "latest_question_list":latest_questions_list
     }
     return HttpResponse(template.render(context,request))
 
-    return HttpResponse(output)
 
+# for the 404 error
 def details(request,question_id):
     try:
         question = Question.objects.get(pk=question_id)
